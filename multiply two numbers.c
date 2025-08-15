e376144a574a55d4898c85d13152eab2eb4002a8
krisha-1805 <krisha.pdeu@gmail.com>
2025-08-15 19:27:28 +0530

    Add files via upload

---
+++ multiply two numbers.c
@@ -0,0 +1,12 @@
+//multiply two numbers
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two values=");
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("Product=%d",c);
+    return 0;
+}
