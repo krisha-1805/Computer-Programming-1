2b1b1b74571274eff0a5cd8ad2a90443d8e24879
krisha-1805 <krisha.pdeu@gmail.com>
2025-08-15 19:27:49 +0530

    Add files via upload

---
+++ divide two numbers.c
@@ -0,0 +1,12 @@
+//divide two numbers
+
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter two values=");
+    scanf("%f %f",&a,&b);
+    c=a/b;
+    printf("Division=%f",c);
+    return 0;
+}
