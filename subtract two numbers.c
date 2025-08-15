d14b82d8668a4c73098f9992e172261b4a35fbff
krisha-1805 <krisha.pdeu@gmail.com>
2025-08-15 19:27:04 +0530

    Add files via upload

---
+++ subtract two numbers.c
@@ -0,0 +1,12 @@
+//subtract two numbers
+
+#include<stdio.h>
+int main()
+{
+   int a,b,sub;
+   printf("Enter two numbers=");
+   scanf("%d %d",&a,&b);
+   sub=a-b;
+   printf("sub=%d",sub);
+   return 0;
+}
