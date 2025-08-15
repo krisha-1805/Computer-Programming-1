3655b98761473736f715332b53c9624c83c53f86
krisha-1805 <krisha.pdeu@gmail.com>
2025-08-15 19:31:57 +0530

    Add files via upload

---
+++ convert kg into grams.c
@@ -0,0 +1,12 @@
+//convert kg into grams
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter value in kg=");
+    scanf("%d",&a);
+    b=a*1000;
+    printf("value of %d kg in grams is %d",a,b);
+    return 0;
+}
